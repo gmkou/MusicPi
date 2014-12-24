d6a186cb212c7c9f8fc415e6f7d62e5c9abaee4b
Kosuke Nagano <gm.charlie@gmail.com>
2014-12-24 18:33:18 +0900

    add server template

---
+++ server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding:utf-8
+from bottle import route, run, template
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
