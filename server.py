53f868bb045aa97cffd4af0864653cde9b41ab60
Kosuke Nagano <gm.charlie@gmail.com>
2015-01-08 01:05:26 +0900

    Modify to use Singleton Design pattern

--- server.py
+++ server.py
@@ -15,14 +15,39 @@ stream_name_to_url = {
     "indie": "http://173.231.136.91:8070/"
 }
 
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        if '_inst' not in vars(cls):
+            cls._inst = super(Singleton, cls).__new__(cls, *args, **kwargs)
+        return cls._inst
+
+class Player(Singleton): 
+    _player = None
+    def __init__(self):
+        pass
+
+    def play(self, channel):
+        self.stop()
+        Player._player = subprocess.Popen(["mplayer", stream_name_to_url[channel]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+    def stop(self):
+        if Player._player is not None:
+            Player._player.kill()
+
 @route('/')
 def index():
-    redirect("/main")
+    return template('list_template', channels=channels)
 
-@route('/<stream_name>')
-def list(stream_name):
-    player = subprocess.Popen(["mplayer", stream_name_to_url[stream_name]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+@route('/<channel>')
+def playchannel(channel):
+    p = Player()
+    p.play(channel)
+    return template('list_template', channels=channels)
 
+@route('/stop')
+def stop():
+    p = Player()
+    p.stop()
     return template('list_template', channels=channels)
 
 run(host='localhost', port=8080, debug=True, reloader=True)
